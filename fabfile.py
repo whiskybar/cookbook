bd019048d405e703e09869dc59452b60c830bec5
Jiri Barton <jbar@hosting4u.cz>
2011-03-13 22:06:53 +0100

    added a deployment script

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from fabric.operations import run
+from fabric.decorators import hosts
+from fabric.context_managers import cd, prefix
+
+@hosts('uvar@uvar.si')
+def deploy():
+    with cd('~/uvar.si'):
+        run('git pull')
+    with prefix('workon uvar'):
+        run('django-admin.py collectstatic --noinput')
+    run('supervisorctl uvar.si restart')
+

9611e811e6651002d67ef9763fb81924ceab288f
Jiri Barton <jbar@hosting4u.cz>
2011-03-14 17:29:57 +0100

    streamlined the fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,10 +6,8 @@ from fabric.context_managers import cd, prefix
 def deploy():
     with cd('~/uvar.si'):
         run('git pull')
-    with prefix('export WORKON_HOME=~/envs'):
-        with prefix('source /usr/local/bin/virtualenvwrapper.sh'):
-            with prefix('workon uvar'):
-                run('django-admin.py syncdb')
-                run('django-admin.py collectstatic --noinput')
+    with prefix('workon uvar'):
+        run('django-admin.py syncdb')
+        run('django-admin.py collectstatic --noinput')
     run('supervisorctl restart uvar.si')
 

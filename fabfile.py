9bf80a023e6a43687cd13c7c16ad6e8395ee068c
Vojtech Jasny <voy@voy.cz>
2011-03-20 17:57:57 +0100

    Quick deploy without db migrations and syncdb.

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,18 @@
 from fabric.operations import run
-from fabric.decorators import hosts
-from fabric.context_managers import cd, prefix
+from fabric.context_managers import cd, prefix, env
 
-@hosts('uvar@uvar.si')
-def deploy():
+env.hosts = ['uvar@uvar.si']
+
+
+def deploy(quick=False):
     with cd('~/uvar.si'):
         run('git pull')
     with prefix('workon uvar'):
-        run('django-admin.py syncdb')
+        if not quick:
+            run('django-admin.py syncdb')
         run('django-admin.py collectstatic --noinput')
     run('supervisorctl restart uvar.si')
 
+
+def quickdeploy():
+    deploy(quick=True)
